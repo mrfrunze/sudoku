c735ba258620b2bf11621125acbe8763c73e7d92
Oleg <stoynov.oleg@gmail.com>
2020-03-26 19:15:01 +0200

    form avtorisation

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    let btn = document.querySelector('.btn').onclick = function(){
+
+        let log = document.querySelector('#input-login').value;
+        let pasw = document.querySelector('#input-pasword').value;
+
+        if(log == "user"){
+            document.location.href = "sudoku.html";
+
+            if(pasw == "1234"){
+                document.location.href = "sudoku.html";
+                alert("Авторизация успешна");
+            }
+        }
+        else{
+            alert("Вы ввели неправильно логин или пароль")
+            document.location.href = "index.html";
+        }
+    }
+}
